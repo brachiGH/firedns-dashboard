9ae6ae0a07e6f27d9440b41ff51921315ffab922
brachigh <40743558+brachiGH@users.noreply.github.com>
2025-03-27 08:21:47 +0100

    refactor: update logo width and height

--- app/ui/firedns-logo.tsx
+++ app/ui/firedns-logo.tsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 export default function FireDNSLogo() {
   return (
     <Link href="/dashboard" className={`flex flex-row items-center leading-none text-white space-x-4 mouse-pointer`}>
-      <Image src="/logo.png" alt="FireDNS Logo" className="h-8 w-8" />
+      <Image width={32} height={32} src="/logo.png" alt="FireDNS Logo" />
       <span className="text-xl font-bold">
-        Fire<span className="text-orange-600">DNS</span>
+      Fire<span className="text-orange-600">DNS</span>
       </span>
     </Link>
   );
